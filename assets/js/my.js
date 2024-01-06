15131fc09d83c0b26036cd9ad23ba699ef25685b
Azure Pipeline <azure-pipeline@coso.com>
2024-01-06 18:24:09 +0100

    add link forwarding exceptions

--- assets/js/my.js
+++ assets/js/my.js
@@ -1,11 +1,11 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 document.addEventListener('DOMContentLoaded', () => {
-    setCopyright();
+  setCopyright();
 }, false);
 
 function setCopyright() {
-    document.getElementById("copyright").innerText = "© " + new Date().getFullYear() + " Corporate Software AG";
+  document.getElementById("copyright").innerText = "© " + new Date().getFullYear() + " Corporate Software AG";
 }
 
 function scrollFunction() {
@@ -21,9 +21,9 @@ function scrollFunction() {
 // Set target to _blank in all external links.
 let links = document.getElementsByTagName("a");
 for (let l of links) {
-    if (l.href.startsWith("http") && !l.href.startsWith("https://www.corporatesoftware.ch")) {
-        l.target = "_blank"
-    }
+  if (l.href.startsWith("http") && !l.href.startsWith("http://localhost:1313") && !l.href.startsWith("https://www.corporatesoftware.ch") && !l.href.startsWith("https://happy-hill-0bae78803.4.azurestaticapps.net")) {
+    l.target = "_blank"
+  }
 }
 
 var successMessage = document.getElementById("success-message");
@@ -31,27 +31,27 @@ var errorMessage = document.getElementById("error-message");
 
 var contactForm = document.getElementById("contact-form");
 if (contactForm) {
-  contactForm.onsubmit = function(event) {
+  contactForm.onsubmit = function (event) {
     event.preventDefault(); // Don't let the browser submit the form.
     var payload = {};
 
     // Build JSON key-value pairs using the form fields.
     contactForm.querySelectorAll("input, textarea").forEach(field => {
-        payload[field.name] = field.value;
+      payload[field.name] = field.value;
     });
 
     // Post the payload to the contact endpoint.
     fetch("https://coso-web.azurewebsites.net/api/WebFormTrigger", {
-        method: 'post',
-        body: JSON.stringify(payload)
+      method: 'post',
+      body: JSON.stringify(payload)
     }).then(resp => {
-        if (!resp.ok) {
-            console.error(resp);
-            return;
-        }
-        // Display success message.
-        successMessage.style.display = "block";
-        contactForm.style.display = "none";
+      if (!resp.ok) {
+        console.error(resp);
+        return;
+      }
+      // Display success message.
+      successMessage.style.display = "block";
+      contactForm.style.display = "none";
     });
   }
 }
